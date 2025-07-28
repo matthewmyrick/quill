bc34c5bbfe8974b3b54b73eac0d70f8a07514f76
Matthew Myrick <matthewmyrick2@gmail.com>
2025-07-28 13:25:37 -0400

    Updating release

--- Formula/quill-task.rb
+++ Formula/quill-task.rb
@@ -1,15 +1,26 @@
 class QuillTask < Formula
   desc "Git-context-aware task management TUI with local and MongoDB storage support"
   homepage "https://github.com/MatthewMyrick/quill"
-  url "https://github.com/MatthewMyrick/quill/releases/download/v0.1.0/quill-task-x86_64-apple-darwin.tar.gz"
-  sha256 "PLACEHOLDER_SHA256" # This will be updated by the GitHub Action
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/MatthewMyrick/quill/releases/download/v0.1.0/quill-task-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_INTEL"
+    else
+      url "https://github.com/MatthewMyrick/quill/releases/download/v0.1.0/quill-task-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_ARM"
+    end
+  end
   license "MIT"
   version "0.1.0"
 
   depends_on "rust" => :build
 
   def install
-    bin.install "quill-task" => "quill"
+    if Hardware::CPU.intel?
+      bin.install "quill-task-x86_64-apple-darwin" => "quill"
+    else
+      bin.install "quill-task-aarch64-apple-darwin" => "quill"
+    end
   end
 
   test do
